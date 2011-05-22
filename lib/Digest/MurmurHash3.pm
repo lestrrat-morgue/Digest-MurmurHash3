8bf6cb516ed92a1697ef24e27bc4095066c57ad8
lestrrat <lestrrat+github@gmail.com>
2011-05-22 23:52:28 +0900

    docs

--- lib/Digest/MurmurHash3.pm
+++ lib/Digest/MurmurHash3.pm
@@ -47,4 +47,44 @@ Digest::MurmurHash3 - MurmurHash3 Implementation For Perl
     # Create two 64 bit pieces (your perl must be built to use 64bit ints)
     my ($v1, $v2) = murmur128_x64( $data_to_hash );
 
+=head1 DESCRIPTION
+
+This module provides an interface to MurmurHash3 functions.
+
+=head1 FUNCTIONS
+
+=head2 $h = murmur32( $data [, $seed] )
+
+Calculates a 32 bit hash.
+
+=head2 @varies = murmur128( $data [, $seed ] )
+
+Calculates a 128 bit hash.
+
+Note that the actual implementation of this function will differ depending 
+on how your perl was built.
+
+If your perl was built with 64 bit integers, this function is an alias to
+mumur128_x64(). Otherwise, it's an alias to mumur128_x68(). While they
+employ similar calculations, each use the best approach for your system so
+the results are different. The x64 version returns two element list of
+64 bit integers. The x86 version returns a four element list of 32 bit
+integers.
+
+=head2 ($v1, $v2, $v3, $v4) = mumur128_x86( $data [, $seed ] )
+
+Calculates a 128 bit hash. The result is returned as a four element list of
+32 bit integers
+
+=head2 ($v1, $v2) = murmur128_x64( $data [, $seed ] )
+
+Calculates a 128 bit hash. The result is returned as a two element list of
+64 bit integers
+
+=head1 AUTHOR
+
+Daisuke Maki C<< <daisuke@endeworks.jp> >>
+
+MurmurHash3 by Austin Appleby.
+
 =cut
