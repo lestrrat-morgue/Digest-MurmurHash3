3c76d4c23bdd8f19d1f8a5f9eedb4ba62474cce4
Daisuke Maki <lestrrat+github@gmail.com>
2013-12-10 20:15:24 +0900

    version++; this be Digest-MurmurHash3-0.02

--- lib/Digest/MurmurHash3.pm
+++ lib/Digest/MurmurHash3.pm
@@ -5,7 +5,7 @@ use Config ();
 use constant HAVE_64BITINT => $Config::Config{use64bitint};
 use XSLoader;
 BEGIN {
-    our $VERSION = '0.01';
+    our $VERSION = '0.02';
     XSLoader::load __PACKAGE__, $VERSION;
 }
 
